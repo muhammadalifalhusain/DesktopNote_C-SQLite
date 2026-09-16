6c3a8fc43007309b42b0a2d8be2ed56bb4eaeb04
MUHAMMAD ALIF AL HUSAIN <112967082+muhammadalifalhusain@users.noreply.github.com>
2026-09-16 07:53:35 +0700

    feat : converter datetime to string

---
+++ DesktopNote/Converters/DateTimeToStringConverter.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+
+namespace DesktopNote.Converters;
+
+public class DateTimeToStringConverter : IValueConverter
+{
+    public object Convert(
+        object value,
+        Type targetType,
+        object parameter,
+        string language)
+    {
+        if (value is DateTime dateTime)
+            return dateTime.ToString("dd MMMM yyyy HH:mm");
+
+        return string.Empty;
+    }
+
+    public object ConvertBack(
+        object value,
+        Type targetType,
+        object parameter,
+        string language)
+    {
+        throw new NotImplementedException();
+    }
+}
