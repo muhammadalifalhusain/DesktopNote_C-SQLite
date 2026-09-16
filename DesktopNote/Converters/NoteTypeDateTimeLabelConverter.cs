07cd819655a77c633d9722565df3f16e943fc738
MUHAMMAD ALIF AL HUSAIN <112967082+muhammadalifalhusain@users.noreply.github.com>
2026-09-16 08:35:01 +0700

    feat : datetime converter

---
+++ DesktopNote/Converters/NoteTypeDateTimeLabelConverter.cs
@@ -0,0 +1,35 @@
+﻿using DesktopNote.Models;
+using Microsoft.UI.Xaml.Data;
+using System;
+
+namespace DesktopNote.Converters;
+
+public class NoteTypeDateTimeLabelConverter : IValueConverter
+{
+    public object Convert(
+    object value,
+    Type targetType,
+    object parameter,
+    string language)
+    {
+        if (value is not NoteType type)
+            return string.Empty;
+    return type switch
+    {
+        NoteType.TASK => "Tenggat",
+        NoteType.RMNDR => "Pengingat",
+        NoteType.MTING => "Jadwal Meeting",
+        _ => string.Empty
+    };
+    }
+
+    public object ConvertBack(
+        object value,
+        Type targetType,
+        object parameter,
+        string language)
+    {
+        throw new NotImplementedException();
+    }
+
+}
---
+++ DesktopNote/Converters/NoteTypeDateTimeVisibilityConverter.cs
@@ -0,0 +1,34 @@
+﻿using DesktopNote.Models;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using System;
+
+namespace DesktopNote.Converters;
+
+public class NoteTypeDateTimeVisibilityConverter : IValueConverter
+{
+    public object Convert(
+    object value,
+    Type targetType,
+    object parameter,
+    string language)
+    {
+        if (value is not NoteType type)
+            return Visibility.Collapsed;
+    return type == NoteType.TASK ||
+           type == NoteType.RMNDR ||
+           type == NoteType.MTING
+        ? Visibility.Visible
+        : Visibility.Collapsed;
+    }
+
+    public object ConvertBack(
+        object value,
+        Type targetType,
+        object parameter,
+        string language)
+    {
+        throw new NotImplementedException();
+    }
+
+}
