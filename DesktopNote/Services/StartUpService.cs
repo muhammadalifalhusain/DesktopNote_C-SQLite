304e4f804f022a4e6acbce83041d0c645fe96854
MUHAMMAD ALIF AL HUSAIN <112967082+muhammadalifalhusain@users.noreply.github.com>
2026-09-18 07:58:23 +0700

    feat : service handle auto start-up

---
+++ DesktopNote/Services/StartUpService.cs
@@ -0,0 +1,43 @@
+﻿//using System;
+//using System.Threading.Tasks;
+//using Windows.ApplicationModel;
+
+//namespace DesktopNote.Services;
+
+//public static class StartupService
+//{
+//    private const string TaskId = "DesktopNoteStartup";
+
+//    public static async Task<bool> IsEnabledAsync()
+//    {
+//        var startupTask =
+//            await StartupTask.GetAsync(TaskId);
+
+//        return startupTask.State ==
+//               StartupTaskState.Enabled;
+//    }
+
+//    public static async Task<bool> EnableAsync()
+//    {
+//        var startupTask =
+//            await StartupTask.GetAsync(TaskId);
+
+//        var state =
+//            await startupTask.RequestEnableAsync();
+
+//        return state ==
+//               StartupTaskState.Enabled;
+//    }
+
+//    public static async Task DisableAsync()
+//    {
+//        var startupTask =
+//            await StartupTask.GetAsync(TaskId);
+
+//        if (startupTask.State ==
+//            StartupTaskState.Enabled)
+//        {
+//            startupTask.Disable();
+//        }
+//    }
+//}
